b6cb424359d4b61a587197441394f0cfeee17bed
kemo <kemo.jallow@hannlync.com>
2024-09-13 14:53:07 +0800

    transcribe sample audios

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
+import time
 
 # Load database URL from environment variable
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/transcribe_ai"
@@ -12,9 +14,24 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    db = None
+    retries = 5
+    while retries > 0:
+        try:
+            db = SessionLocal()
+            yield db
+            db.close()
+            break
+        except OperationalError:
+            retries -= 1
+            time.sleep(5)  # Wait before retrying
+    if db is None:
+        raise RuntimeError("Could not connect to the database after several attempts.")
